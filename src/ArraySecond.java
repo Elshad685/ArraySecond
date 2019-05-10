4c345281cadcd74e09ee7fa16718b8c678399d53
elshad685 <Tomik2018>
2019-05-10 12:50:19 +0400

    Coxlu Matris

--- src/ArraySecond.java
+++ src/ArraySecond.java
@@ -6,16 +6,20 @@ public class ArraySecond {
     public static void main(String[] args) {
 
 
-        ArrayList<String> group=new ArrayList<String>();
+        ArrayList<String> group = new ArrayList<String>();
         group.add("Elshad");
         group.add("Tamerlan");
         group.add("Elshen");
-        System.out.println(group.get(2));
+
+
+        for (String s : group) {
+
+            System.out.println(s);
+        }
+
 
     }
 }
-
-
 
 
 
