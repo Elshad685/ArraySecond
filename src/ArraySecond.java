82b66726c107d0dd3bf70dab34cd6d313fce219e
elshad685 <Tomik2018>
2019-05-10 12:13:28 +0400

    Coxlu Matris

---
+++ src/ArraySecond.java
@@ -0,0 +1,23 @@
+import javax.xml.transform.sax.SAXSource;
+import java.util.ArrayList;
+
+public class ArraySecond {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> group=new ArrayList<String>();
+        group.add("Elshad");
+        group.add("Tamerlan");
+        group.add("Elshen");
+        System.out.println(group.get(2));
+
+    }
+}
+
+
+
+
+
+
+
---
+++ src/ToplaArr.java
@@ -0,0 +1,11 @@
+public class ToplaArr {
+
+    public double total;
+
+
+    public static double Mee(double x) {
+
+        return x;
+
+    }
+}
